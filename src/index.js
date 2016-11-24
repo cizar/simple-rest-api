f82bfdb38cbf6fa0dd34f589bc018534c172d061
Cesar Kastli <cesarkastli@gmail.com>
2016-11-24 08:16:09 -0300

    added a handy console message on start

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import api from './api'
 const server = http.createServer(api)
 
 server.listen(process.env.PORT || 9000, () => {
-  console.log('Listening on port %d', server.address().port)
+  let addr = server.address()
+  let hostname = addr.family === 'IPv6' ? `[${addr.address}]` : addr.address
+  console.log('API is running! Try it out here... http://%s:%s/api/items', hostname, addr.port)
 })
