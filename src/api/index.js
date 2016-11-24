1d5b52f966615b8545346920e258e3cdcc9ebf59
Cesar Kastli <cesarkastli@gmail.com>
2016-11-24 01:27:55 -0300

    Add CORS support

--- src/api/index.js
+++ src/api/index.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import cors from 'cors'
 import bodyParser from 'body-parser'
 import router from './router'
 
 const app = express()
 
+app.use(cors())
 app.use(bodyParser.json())
 
 app.use('/api', router)
