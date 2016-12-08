4fa821b934e0e6e98fafa585460d4edd4cc88e43
Cesar Kastli <cesarkastli@gmail.com>
2016-12-08 17:47:26 -0300

    Allow to configure bind address

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import api from './api'
 
 const server = http.createServer(api)
 
-server.listen(process.env.PORT || 9000, () => {
+const port = process.env.PORT || 9000
+const host = process.env.HOST || 'localhost'
+
+server.listen(port, host, () => {
   let addr = server.address()
   let hostname = addr.family === 'IPv6' ? `[${addr.address}]` : addr.address
   console.log('API is running! Try it out here... http://%s:%s/api/items', hostname, addr.port)
