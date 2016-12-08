773b81369d957e6105feb3412881625c66062abe
Cesar Kastli <cesarkastli@gmail.com>
2016-12-08 17:47:46 -0300

    add simple validation

--- src/api/services/items.js
+++ src/api/services/items.js
@@ -2,7 +2,16 @@ import shortid from 'shortid'
 
 let items = []
 
-export const findAll = () => Promise.resolve(items)
+const validateItem = (item) => {
+  const errors = {}
+  if (!item.name) errors.name = 'Is required'
+  const isValid = Object.keys(errors).length === 0
+  return { isValid, errors }
+}
+
+export const findAll = () => (
+  Promise.resolve(items)
+)
 
 export const findById = (id) => new Promise(resolve => {
   let result = items.find(item => item.id == id)
@@ -10,11 +19,13 @@ export const findById = (id) => new Promise(resolve => {
   resolve(result)
 })
 
-export const insert = (item) => {
+export const insert = (item) => new Promise((resolve, reject) => {
+  const { isValid, errors } = validateItem(item)
+  if (!isValid) return reject(errors)
   item.id = shortid.generate()
   items.push(item)
-  return Promise.resolve(item)
-}
+  resolve(item)
+}) 
 
 export const update = (id, item) => {
   let pos = items.findIndex(item => item.id == id)
