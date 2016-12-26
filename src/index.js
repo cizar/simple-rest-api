ef69de8107c8ebf58de2138661544f624640d2d4
Cesar Kastli <cesarkastli@gmail.com>
2016-12-26 17:53:46 -0300

    Add docker descriptor

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import api from './api'
 const server = http.createServer(api)
 
 const port = process.env.PORT || 9000
-const host = process.env.HOST || 'localhost'
 
-server.listen(port, host, () => {
-  let addr = server.address()
-  let hostname = addr.family === 'IPv6' ? `[${addr.address}]` : addr.address
-  console.log('API is running! Try it out here... http://%s:%s/api/items', hostname, addr.port)
+server.listen(port, () => {
+  console.log('Server is running on port %d', server.address().port)
 })
